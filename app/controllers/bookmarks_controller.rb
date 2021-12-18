dc6965b9acfe65d4f7f40b4fd868178b85d94a6f
Sanae Benali <sanae.benali@gmail.com>
2021-12-18 15:34:18 +0100

    adding the views and controllers

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,29 @@
+class BookmarksController < ApplicationController
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+     render :new
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list)
+    # redirect_to bookmark_path
+  end
+
+  private
+  def bookmark_params
+    params.require(:bookmark).permit(:movie_id, :list_id, :comment)
+  end
+end
